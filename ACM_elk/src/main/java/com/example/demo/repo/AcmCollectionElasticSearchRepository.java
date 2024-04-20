0877dc365a9376273cff375377d1e9c7284479b4
NfikhaOumayma <nfikhaoumayma16042002@gmail.com>
2024-04-20 17:34:45 +0100

    delete setters and getters from ACMCollection

--- ACM_elk/src/main/java/com/example/demo/repo/AcmCollectionElasticSearchRepository.java
+++ ACM_elk/src/main/java/com/example/demo/repo/AcmCollectionElasticSearchRepository.java
@@ -1,4 +1,5 @@
 package com.example.demo.repo;
+
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -17,45 +18,30 @@ import org.springframework.stereotype.Repository;
 import com.example.demo.entity.AcmCollection;
 
 @Repository
-public interface AcmCollectionElasticSearchRepository extends JpaRepository<AcmCollection, Long>{
+public interface AcmCollectionElasticSearchRepository extends JpaRepository<AcmCollection, Long> {
 
-	//edit by youssef boughrara
+	@Query(value = "SELECT new com.example.demo.dto.VoletCollectionByCollectionType(count(*), c.collectionType) "
+			+ "FROM AcmCollection c" + " GROUP BY c.collectionType"
+			+ " ORDER BY c.collectionType DESC ")
+	List<VoletCollectionByCollectionType> getNombreDossierByCollectionType();
 
-			@Query(value = "SELECT new com.example.demo.dto.VoletCollectionByCollectionType(count(*), c.collectionType) "
-					+ "FROM AcmCollection c"
-					+ " GROUP BY c.collectionType"
-					+ " ORDER BY c.collectionType DESC ")
-		List<VoletCollectionByCollectionType> getNombreDossierByCollectionType();
-			
-			
-			@Query(value = "SELECT new com.example.demo.dto.VoletCollectionByOwnerName(count(*), c.ownerName) "
-					+ "FROM AcmCollection c"
-					+ " GROUP BY c.ownerName"
-					+ " ORDER BY c.ownerName DESC ")
-		
-		List<VoletCollectionByOwnerName> getNombreDossierByOwnerName();
-			
-			@Query(value = "SELECT new com.example.demo.dto.VoletCollectionByCustomerName(count(*), c.customerName) "
-					+ "FROM AcmCollection c"
-					+ " GROUP BY c.customerName"
-					+ " ORDER BY c.customerName DESC ")
-		List<VoletCollectionByCustomerName> getNombreDossierByCustomerName();
-			
-			
-			@Query(value = "SELECT new com.example.demo.dto.VoletCollectionByStatut(count(*), c.statutLibelle) "
-					+ "FROM AcmCollection c"
-					+ " GROUP BY c.statutLibelle"
-					+ " ORDER BY c.statutLibelle DESC ")
-		List<VoletCollectionByStatut> getNombreDossierByStatut();      
-			
-			
-			
-			
-			@Query(value = "SELECT new com.example.demo.dto.VoletCollectionByBranch(count(*), c.branchDescription) "
-				+ "FROM AcmCollection c"
-				+ " GROUP BY c.branchDescription"
-				+ " ORDER BY c.branchDescription DESC ")
-		List<VoletCollectionByBranch> getNombreDossierByBranch(); 
+	@Query(value = "SELECT new com.example.demo.dto.VoletCollectionByOwnerName(count(*), c.ownerName) "
+			+ "FROM AcmCollection c" + " GROUP BY c.ownerName" + " ORDER BY c.ownerName DESC ")
+
+	List<VoletCollectionByOwnerName> getNombreDossierByOwnerName();
+
+	@Query(value = "SELECT new com.example.demo.dto.VoletCollectionByCustomerName(count(*), c.customerName) "
+			+ "FROM AcmCollection c" + " GROUP BY c.customerName"
+			+ " ORDER BY c.customerName DESC ")
+	List<VoletCollectionByCustomerName> getNombreDossierByCustomerName();
+
+	@Query(value = "SELECT new com.example.demo.dto.VoletCollectionByStatut(count(*), c.statutLibelle) "
+			+ "FROM AcmCollection c" + " GROUP BY c.statutLibelle"
+			+ " ORDER BY c.statutLibelle DESC ")
+	List<VoletCollectionByStatut> getNombreDossierByStatut();
+
+	@Query(value = "SELECT new com.example.demo.dto.VoletCollectionByBranch(count(*), c.branchDescription) "
+			+ "FROM AcmCollection c" + " GROUP BY c.branchDescription"
+			+ " ORDER BY c.branchDescription DESC ")
+	List<VoletCollectionByBranch> getNombreDossierByBranch();
 }
-			
-			
