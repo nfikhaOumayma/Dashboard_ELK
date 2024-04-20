8189b0f23d105f5e459b8ea3d240f6940876bed7
NfikhaOumayma <nfikhaoumayma16042002@gmail.com>
2024-04-20 17:52:37 +0100

    ajout proposition de query dans customersRep

--- ACM_elk/src/main/java/com/example/demo/repo/ACMCustomerRep.java
+++ ACM_elk/src/main/java/com/example/demo/repo/ACMCustomerRep.java
@@ -12,4 +12,20 @@ public interface ACMCustomerRep extends JpaRepository<AcmCustomer, Long> {
 
 	@Query("SELECT  new com.example.demo.dto.CustomersName( c.id) " + "FROM  AcmCustomer c ")
 	List<CustomersName> findAllCustomers();
+	
+	
+	/*find all customers by gender
+	
+	SELECT count(*)  as  clientActif, [GENDER]
+			  FROM [ACM].[dbo].[ACM_CUSTOMER] c
+			  INNER JOIN [ACM].[dbo].[ACM_LOAN] l
+			  ON c.ID_ACM_CUSTOMER =l.CUSTOMER_ID
+			  group by gender*/
+
+
+			
+	/*find all active customers by gender
+	  SELECT count(*) as clients, [GENDER]
+			  FROM [ACM].[dbo].[ACM_CUSTOMER] 
+			  group by gender*/
 }
